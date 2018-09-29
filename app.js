2ea728101266004731a8f4d500e89d666db73d97
nycuma <31799327+nycuma@users.noreply.github.com>
2018-09-29 15:14:10 +0200

    moved database connection props to separate file

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const index = require('./routes/index')
-const user = require('./routes/user')
+const projectProps = require('./utils/project-properties.json');
+const indexRoute = require('./routes/index')
+const userRoute = require('./routes/user')
 
-// ### Insert personal DB username and password ###
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds215093.mlab.com:15093/freelance-suite-db', { useNewUrlParser: true })
+let dbCon = projectProps.databaseConnection;
+mongoose.connect(`mongodb://${dbCon.username}:${dbCon.password}@${dbCon.uri}`, { useNewUrlParser: true })
 // Get Mongoose to use the global promise library
 mongoose.Promise = global.Promise
-//Get the default connection
 var db = mongoose.connection
-//Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 
@@ -21,8 +20,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 
-app.use('/', index)
-app.use('/api/user', user)
+app.use('/', indexRoute)
+app.use('/api/user', userRoute)
 
 
 // listen for requests
