0afc8a445d24829e51570ac5958c5f150078389c
nycuma <31799327+nycuma@users.noreply.github.com>
2018-09-29 23:29:29 +0200

    moved logging function to middleware.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,10 +1,5 @@
 const User = require('../models/user')
 
-exports.logRequest = (req, res, next) => {
-  console.log('Incomming request to ' + req.originalUrl + ' on ' + new Date())
-  next()
-}
-
 exports.getAllUsers = (req, res) => {
   User.find().exec((err, users) => {
     if (err) {
