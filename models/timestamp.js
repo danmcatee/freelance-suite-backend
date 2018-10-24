a6338058731a1cbcb36a506e70273232352739e8
nycuma <31799327+nycuma@users.noreply.github.com>
2018-10-24 22:51:32 +0200

    replaced attribute isStart by reference to start timestamp

--- models/timestamp.js
+++ models/timestamp.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+/**
+ * startId: a reference to the start timestamp if document is an end timestamp; 
+ *          is null, if document is a start timestamp
+ * timestamp: current point in time
+ * taskId: the ID of the task this timestamp belongs to
+ */
 const TimestampSchema = new Schema({
-  isStart: { type: Boolean, required: true, default: true },
+  startId: { type: Schema.Types.ObjectId },
   timestamp: { type: Date, required: true, default: Date.now },
   taskId: { type: Schema.Types.ObjectId, required: true }
 })
